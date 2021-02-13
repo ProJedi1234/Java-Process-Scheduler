7abefdbc7091d4c93a997e9839ab153c0c91f7ef
Aditya Dhar <adhar2032@gmsil.vom>
2021-02-12 17:00:47 -0800

    Added CSVFile to Scheduler

--- Scheduler.java
+++ Scheduler.java
@@ -1,9 +1,11 @@
 import java.util.List;
 
 public abstract class Scheduler {
+    CSVFile csvFile;
     List<Process> processList;
 
     public Scheduler(List<Process> processList) {
+        this.csvFile = new CSVFile();
         this.processList = processList;
     }
     public abstract void runScheduler();
